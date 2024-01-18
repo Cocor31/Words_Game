1032517d2943bb26cd9532a6e0d19d4c3aeb7534
Cocor_Debian <cocor31@live.fr>
2024-01-18 19:56:13 +0100

    new role 'owner' take in account in roleCheck to allow user to modify his datas

--- server/middlewares/roleCheck.js
+++ server/middlewares/roleCheck.js
@@ -9,7 +9,7 @@ const roleCheck = (...authRoles) => {
         const isAutorized = userRolesChecks.find(val => val === true)
 
         if (!isAutorized) {
-            // S'il y a un autorisation owner on regarde si le user est le propriétaire de la donnée
+            // S'il y a une autorisation owner on regarde si le user est le propriétaire de la donnée
             if (authRoles.includes("owner")) {
                 const pid = parseInt(req.params.id)
                 const userId = req.userID
